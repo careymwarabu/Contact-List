4f90a9426d9c045a7929aa6cbfc2bdabc67cc2ce
careymwarabu <careyakmwarabu@gmail.com>
2021-05-29 09:56:54 +0300

    add contact.py

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    contact_list = [] # Empty contact list
+    # Init method up here
+    def save_contact(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        Contact.contact_list.append(self)
+
+    def __init__(self,first_name,last_name,number,email):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
+
+    @classmethod
+    def display_contacts(cls):
+        '''
+        method that returns the contact list
+        '''
+        return cls.contact_list
